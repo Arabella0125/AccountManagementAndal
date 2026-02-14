200865e6b4a1f59729bc597914bdbe9eca685c58
Arabella <andalarabellamae@gmail.com>
2026-02-14 12:47:08 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AccountManagementAndal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Account Management System");
+
+            string username = "arabella";
+            string password = "andal123";
+
+            Console.Write("Enter username: ");
+            string usernameInput = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string passwordInput = Console.ReadLine();
+
+            if (usernameInput == username && passwordInput == password)
+            {
+                Console.WriteLine("Login Successful!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect info. Please, try again!");
+
+            }
+        }
+    }
+}
